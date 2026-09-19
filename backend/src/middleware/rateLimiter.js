57d4a6a2335e5a6751ba72888b1c37892874d4fe
RoycetheGreat2 <23-05755@g.batstate-u.edu.ph>
2026-09-19 12:25:55 +0800

    initial commit, backend done

---
+++ backend/src/middleware/rateLimiter.js
@@ -0,0 +1,18 @@
+import ratelimit from "../config/upstash.js"
+
+const rateLimiter = async (req, res, next) => {
+    try{
+        const {success} = await ratelimit.limit(req.ip);
+        if(!success){
+            return res.status(429).json({error: "Too Many Requests"});
+        }
+        next();
+    }catch(error){
+        console.error("Rate limit error:", error);
+        res.status(500).json({error: "Internal Server Error"});
+        next(error);
+    }
+}
+
+export default rateLimiter;
+
